74f12af22df5576b9c717163e1e264dbb3ed90ee
aviralrabbit1 <aviral.robin@gmail.com>
2023-02-05 01:44:26 +0530

    Signup UI fixed

--- app/src/main/java/com/example/mvp5/CreateAccount.java
+++ app/src/main/java/com/example/mvp5/CreateAccount.java
@@ -2,7 +2,10 @@ package com.example.mvp5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        TextView RedirectToLoginPage = findViewById(R.id.login_text_view_btn);
+        RedirectToLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(CreateAccount.this, LoginActivity.class);
+                startActivity(intent2);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mvp5/LoginActivity.java
+++ app/src/main/java/com/example/mvp5/LoginActivity.java
@@ -2,13 +2,27 @@ package com.example.mvp5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView RedirectToSignupPage = findViewById(R.id.create_account_text_view_btn);
+        RedirectToSignupPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(LoginActivity.this, CreateAccount.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
