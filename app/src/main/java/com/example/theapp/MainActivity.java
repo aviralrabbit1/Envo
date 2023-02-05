99e9df8b59b8a7df86b671b6f872a2001a0d7b40
aviralrabbit1 <aviral.robin@gmail.com>
2023-02-05 14:04:50 +0530

    background video activity running

---
+++ app/src/main/java/com/example/theapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.theapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonToLogin = (Button) findViewById(R.id.buttonToLogin);
+        buttonToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
